52c07969e1b2adac860bd1e24752daa264ff7d62
laurelcerrato <laurelcerrato@gmail.com>
2023-09-18 19:52:46 -0600

    chamged file

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const http = require('http');
+const express = require('express');
+const app = express();
  
-const hostname = '127.0.0.1';
-const port = 3000;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Lau Cerrato');
+app.get('/', (req, res) => {
+  res.send("Lau Cerrato");
 });
  
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
